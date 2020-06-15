fd24c329779ba49afb1886ebe7033bc1270e4577
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2020-06-15 18:14:55 +0000

    KAuth: Convert license headers to SPDX expressions

--- cmake/FindPolkitQt.cmake
+++ cmake/FindPolkitQt.cmake
@@ -13,35 +13,11 @@
 # standard syntax, e.g. find_package(PolkitQt 1.0)
 # For compatibility, this can also be done by setting the POLKITQT_MIN_VERSION variable.
 
-# Copyright (c) 2009, Daniel Nicoletti, <dantti85-pk@yahoo.com.br>
-# Copyright (c) 2009, Dario Freddi, <drf54321@gmail.com>
-# Copyright (c) 2009, Michal Malek, <michalm@jabster.pl>
-# Copyright (c) 2009, Alexander Neundorf, <neundorf@kde.org>
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-# 1. Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-# 2. Redistributions in binary form must reproduce the above copyright
-#    notice, this list of conditions and the following disclaimer in the
-#    documentation and/or other materials provided with the distribution.
-# 3. Neither the name of the University nor the names of its contributors
-#    may be used to endorse or promote products derived from this software
-#    without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
-# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
-# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
-# SUCH DAMAGE.
-
+# SPDX-FileCopyrightText: 2009 Daniel Nicoletti <dantti85-pk@yahoo.com.br>
+# SPDX-FileCopyrightText: 2009 Dario Freddi <drf@kde.org>
+# SPDX-FileCopyrightText: 2009 Michal Malek <michalm@jabster.pl>
+# SPDX-FileCopyrightText: 2009 Alexander Neundorf <neundorf@kde.org>
+# SPDX-License-Identifier: BSD-3-Clause
 
 # Support POLKITQT_MIN_VERSION for compatibility:
 if(NOT PolkitQt_FIND_VERSION)
--- src/backends/polkit-1/Polkit1Backend.cpp
+++ src/backends/polkit-1/Polkit1Backend.cpp
@@ -1,23 +1,10 @@
 /*
-*   Copyright (C) 2008 Nicola Gigante <nicola.gigante@gmail.com>
-*   Copyright (C) 2009 Radek Novacek <rnovacek@redhat.com>
-*   Copyright (C) 2009-2010 Dario Freddi <drf@kde.org>
-*   Copyright (C) 2020 David Edmundson <davidedmundson@kde.org>
-*
-*   This program is free software; you can redistribute it and/or modify
-*   it under the terms of the GNU Lesser General Public License as published by
-*   the Free Software Foundation; either version 2.1 of the License, or
-*   (at your option) any later version.
-*
-*   This program is distributed in the hope that it will be useful,
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*   GNU General Public License for more details.
-*
-*   You should have received a copy of the GNU Lesser General Public License
-*   along with this program; if not, write to the
-*   Free Software Foundation, Inc.,
-*   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA .
+    SPDX-FileCopyrightText: 2008 Nicola Gigante <nicola.gigante@gmail.com>
+    SPDX-FileCopyrightText: 2009 Radek Novacek <rnovacek@redhat.com>
+    SPDX-FileCopyrightText: 2009-2010 Dario Freddi <drf@kde.org>
+    SPDX-FileCopyrightText: 2020 David Edmundson <davidedmundson@kde.org>
+
+    SPDX-License-Identifier: LGPL-2.1-or-later
 */
 
 #include "Polkit1Backend.h"
